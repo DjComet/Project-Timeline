c619c1cbff9696a0ad54d1ed1b055dc8ed5a3b7b
Alberto <alberto.abad.sanz@gmail.com>
2018-05-28 03:24:09 +0200

    Activated Physics drop script

---
+++ Project Timeline/Assets/Scripts/Platforms, Objects and such/Moving Objects/ActivatedPhysicsDrop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatedPhysicsDrop : MonoBehaviour {
+
+    TimeLine timeLine;
+    Rigidbody rb;
+    public GameObject connectedTo;
+
+    private Linker linker;
+    bool doneActivated = false;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        if (connectedTo!= null)
+        linker = connectedTo.GetComponent<Linker>();
+        rb.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (connectedTo != null)
+        {
+            if (!linker.active && !doneActivated)
+            {
+                rb.isKinematic = true;
+            }
+
+            if (linker.active)
+            {
+                rb.isKinematic = false;
+                doneActivated = true;
+            }
+        }
+	}
+}
